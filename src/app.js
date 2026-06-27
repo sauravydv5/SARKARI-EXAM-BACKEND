e927d22f70214e56c815f1c061a89e88e1b306e7
Saurav Kumar <sauravkumar91937@gmail.com>
2026-06-27 16:25:31 +0530

    fix error

--- src/app.js
+++ src/app.js
@@ -21,45 +21,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // CORS
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:5173",
-//       "https://sarkarijobhub.website",
-//       "https://www.sarkarijobhub.website",
-//     ],
-//     credentials: true,
-//   })
-// );
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://localhost:5175",
-  "https://sarkarijobhub.website",
-  "https://www.sarkarijobhub.website",
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      console.log("Origin:", origin);
-
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      console.log("Blocked Origin:", origin);
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://sarkarijobhub.website",
+      "https://www.sarkarijobhub.website",
+    ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-app.options("*", cors());
 
 // Middlewares
 app.use(express.json());
